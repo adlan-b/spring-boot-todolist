fee377b84b5a47bb9d81e2a98883cfacd9955146
Adlan Kha <amartin0950@gmail.com>
2022-05-24 20:56:38 +0200

    add localdate fields

--- ToDoList/src/main/java/main/model/ToDo.java
+++ ToDoList/src/main/java/main/model/ToDo.java
@@ -1,9 +1,12 @@
 package main.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
-@Table(name= "todo")
+@Table(name= "todos")
 public class ToDo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,6 +15,12 @@ public class ToDo {
     private String description;
     private boolean completed;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createDate = LocalDate.now();
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate editDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate deadlineDate;
 
     public ToDo() {
     }
@@ -53,6 +62,30 @@ public class ToDo {
 
     public void setCompleted(boolean completed) {
         this.completed = completed;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDate getEditDate() {
+        return editDate;
+    }
+
+    public void setEditDate(LocalDate editDate) {
+        this.editDate = editDate;
+    }
+
+    public LocalDate getDeadlineDate() {
+        return deadlineDate;
+    }
+
+    public void setDeadlineDate(LocalDate deadlineDate) {
+        this.deadlineDate = deadlineDate;
     }
 
     @Override
